219e52f3de5859bd7078fc092f15d5ce4020dff2
apsence <moderor.533@gmail.com>
2026-09-05 10:49:10 +0300

    Added student info and Pi calculation

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const http = require('http');
+
+function calculatePi(digits) {
+    let pi = 3.0;
+    let sign = 1;
+    for (let i = 2; i < 200000; i += 2) {
+        pi += sign * (4.0 / (i * (i + 1) * (i + 2)));
+        sign *= -1;
+    }
+    return pi.toFixed(digits);
+}
+
 const server = http.createServer((req, res) => {
-res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-res.end('<h1>Привет, мир!</h1>');
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+
+    const journalNumber = 6; 
+    const calculatedPi = calculatePi(journalNumber);
+
+    res.write('Гурецкий Иван Сергеевич<br>');
+    res.write('Группа: 401<br>');
+    res.write(`Число Пи (знаков после запятой — ${journalNumber}): ${calculatedPi}`);
+    
+    res.end();
 });
+
 const PORT = 3000;
 server.listen(PORT, () => {
-console.log(`Сервер запущен на http://localhost:${PORT}`);
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
